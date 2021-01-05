ed9b4a9db70a115fa336097e713de2db496e360a
Kian Ting <kian.ting@middleware.co.nz>
2021-01-05 16:54:22 +1300

    try to do error handling when http.send crapt itself

--- ForgerockClientTest.groovy
+++ ForgerockClientTest.groovy
@@ -1,8 +1,9 @@
-import org.forgerock.http.Client
+
 import org.forgerock.http.handler.HttpClientHandler
 import org.forgerock.http.protocol.Request
 import org.forgerock.util.AsyncFunction
 import org.forgerock.http.protocol.Status
+import org.forgerock.http.Client
 
 def getjwks(jwksurl) {
 	def http = new Client(new HttpClientHandler())
@@ -17,22 +18,27 @@ println "Beginning of test code"
 
 
 def sJwks
-return getjwks("https://httpbin.org/get").then { response -> 
+
+return getjwks("https://httpbin.org/get").thenAsync(response -> {
 	if(response != null && !response.entity.isRawContentEmpty()) {
 		sJwks = response.entity.toString()
 		if(Status.OK == response.status) {
 			println("Success response: " + sJwks)
 		}else {
 			println("Failure response: " + sJwks)
+			return
 		}
 		response.close()
 	}else {
 		println "Response is empty!!!"
 	}
-}.thenAsync({
        println("Here we call the next.handle(context, request)")
 	   println("sJwks is still reacheable : " + sJwks)
-} as AsyncFunction)
-	   
+})
+.thenCatchAsync(error -> {
+	  println("AN ERROR OCCURED !!!!")
+	  error.printStackTrace()
+})
+   
 
 println "This line will never be reached!!!!"
